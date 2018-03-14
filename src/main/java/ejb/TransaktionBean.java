04c4159cdfe6160dcb971666e877d50e61990eec
DHBWDev <36128457+DHBWDev@users.noreply.github.com>
2018-03-14 12:26:16 +0100

    XML-Import

--- src/main/java/ejb/TransaktionBean.java
+++ src/main/java/ejb/TransaktionBean.java
@@ -1,8 +1,19 @@
 package ejb;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.Stateless;
+import jpa.Benutzer;
+import jpa.Kategorie;
 import jpa.Transaktion;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
 
 @Stateless
 public class TransaktionBean extends EntityBean<Transaktion, Long> {
@@ -13,6 +24,62 @@ public class TransaktionBean extends EntityBean<Transaktion, Long> {
 
     public List<Transaktion> findeAlle() {
         return this.em.createQuery("SELECT c FROM Transaktion").getResultList();
+    }
+
+    //importiert Transaktionen aus einer XML-File
+    public void importiereXML() {
+        Document doc = null;
+        File f = new File("Transaktionen.xml");
+
+        try {
+            // Das Dokument erstellen
+            SAXBuilder builder = new SAXBuilder();
+            doc = builder.build(f);
+            XMLOutputter fmt = new XMLOutputter();
+
+            // Wurzelelement ausgeben
+            Element root = doc.getRootElement();
+
+            List<Element> children = root.getChildren();
+    
+            int i = 0;
+            while (i < children.size()) {
+                Transaktion t = new Transaktion();
+
+                Element bezeichnung = children.get(i).getChild("Bezeichnung");
+
+                Element beschreibung = children.get(i).getChild("Beschreibung");
+
+                Element betrag = children.get(i).getChild("Betrag");
+
+                Element erstellungsDatum = children.get(i).getChild("ErstellungsDatum");
+
+                Element art = children.get(i).getChild("Art");
+
+                Element benutzer = children.get(i).getChild("Benutzer");
+
+                //Wenn Kategorie noch nicht vorhanden, automatisch anlegen
+                //Kategorie mitgeben welche Art
+                Element kategorie = children.get(i).getChild("Kategorie");
+
+                //XML-Werte dem Objekte übergeben
+                t.setArt(art.getValue());
+                t.setBenutzer(null);
+                t.setBeschreibung(beschreibung.getValue());
+                t.setBezeichnung(bezeichnung.getValue());
+                t.setErstellungsDatum(null);
+                t.setKategorie(null);
+
+                this.speichernNeu(t);
+
+                i++;
+
+            }
+
+        } catch (JDOMException | IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/main/java/jpa/Transaktion.java
+++ src/main/java/jpa/Transaktion.java
@@ -6,31 +6,105 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
-import jpa.Benutzer;
+import javax.persistence.TableGenerator;
 import jpa.Kategorie;
 
 @Entity
-public class Transaktion implements Serializable{
+public class Transaktion implements Serializable {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "transaktion_ids")
+    @TableGenerator(name = "transaktion_ids", initialValue = 0, allocationSize = 1)
     private long id;
-    
-    
-    @NotNull(message = "Die Bezeichnung darf nicht leer sein.")
-    String bezeichnung;
-    
-    String beschreibung;
-    
-    @NotNull(message = "Das Datum darf nicht leer sein.")  
-    Date erstellungsDatum;
-    
-    @NotNull(message = "Die Art darf nicht leer sein")  
-    String art;
-    
+
+    //@NotNull(message = "Die Bezeichnung darf nicht leer sein.")
+    String bezeichnung = "";
+
+    String beschreibung = "";
+
+    //@NotNull(message = "Das Datum darf nicht leer sein.")  
+    Date erstellungsDatum = new Date(System.currentTimeMillis());
+
+    //@NotNull(message = "Die Art darf nicht leer sein")  
+    String art = "";
+
     @ManyToOne
     Benutzer benutzer = null;
-    
+
     @ManyToOne(optional = true)
     Kategorie kategorie = null;
+
+    //<editor-fold defaultstate="collapsed" desc="Konstruktoren">
+    public Transaktion(String bezeichnung, String beschreibung, Date erstellungsDatum, String art, Benutzer benutzer, Kategorie kategorie) {
+        this.bezeichnung = bezeichnung;
+        this.beschreibung = beschreibung;
+        this.erstellungsDatum = erstellungsDatum;
+        this.art = art;
+        this.benutzer = benutzer;
+        this.kategorie = kategorie;
+    }
+
+    public Transaktion() {
+
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Setter und Getter">
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public Date getErstellungsDatum() {
+        return erstellungsDatum;
+    }
+
+    public void setErstellungsDatum(Date erstellungsDatum) {
+        this.erstellungsDatum = erstellungsDatum;
+    }
+
+    public String getArt() {
+        return art;
+    }
+
+    public void setArt(String art) {
+        this.art = art;
+    }
+
+    public Benutzer getBenutzer() {
+        return benutzer;
+    }
+
+    public void setBenutzer(Benutzer benutzer) {
+        this.benutzer = benutzer;
+    }
+
+    public Kategorie getKategorie() {
+        return kategorie;
+    }
+
+    public void setKategorie(Kategorie kategorie) {
+        this.kategorie = kategorie;
+    }
+    //</editor-fold>
+
 }
