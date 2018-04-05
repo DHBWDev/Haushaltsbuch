18cb935d9996a09b1b0ab9ab5a5296c492669cce
DHBWDev <36128457+DHBWDev@users.noreply.github.com>
2018-04-05 10:23:22 +0200

    ID-Erzeugung der Transaktionen mittels UUID

--- src/main/java/ejb/TransaktionBean.java
+++ src/main/java/ejb/TransaktionBean.java
@@ -27,7 +27,7 @@ public class TransaktionBean extends EntityBean<Transaktion, Long> {
 
     @EJB
     KategorieBean kategorieBean;
-    
+
     @EJB
     BenutzerBean benutzerBean;
 
@@ -43,8 +43,7 @@ public class TransaktionBean extends EntityBean<Transaktion, Long> {
         CriteriaQuery<Transaktion> query = cb.createQuery(Transaktion.class);
         Root<Transaktion> from = query.from(Transaktion.class);
         query.select(from);
-        
-       
+
         // ORDER BY erstellungsDatum, erstellungsDatum
         query.orderBy(cb.asc(from.get("erstellungsDatum")), cb.asc(from.get("erstellungsDatum")));
 
@@ -60,120 +59,119 @@ public class TransaktionBean extends EntityBean<Transaktion, Long> {
 
         return em.createQuery(query).getResultList();
     }
-    
-    public List<Transaktion> findeAlle(Date vonDatum, Date bisDatum, TransaktionsArten art){
+
+    public List<Transaktion> findeAlle(Date vonDatum, Date bisDatum, TransaktionsArten art) {
         return em.createQuery("SELECT t FROM Transaktion t"
-        + " WHERE (t.benutzer = :benutzer)"
-        + " AND (t.art = :art)"
-        + " AND (t.erstellungsDatum >= :vonDatum) AND (t.erstellungsDatum <= :bisDatum)")
-        .setParameter("benutzer", benutzerBean.gibAktuellenBenutzer())
-        .setParameter("art", art)
-        .setParameter("vonDatum", vonDatum)
-        .setParameter("bisDatum", bisDatum)
-        .getResultList();
+                + " WHERE (t.benutzer = :benutzer)"
+                + " AND (t.art = :art)"
+                + " AND (t.erstellungsDatum >= :vonDatum) AND (t.erstellungsDatum <= :bisDatum)")
+                .setParameter("benutzer", benutzerBean.gibAktuellenBenutzer())
+                .setParameter("art", art)
+                .setParameter("vonDatum", vonDatum)
+                .setParameter("bisDatum", bisDatum)
+                .getResultList();
     }
-    
-    public List<Transaktion> findeAlle(Date vonDatum, Date bisDatum, TransaktionsArten art, Kategorie kategorie){
+
+    public List<Transaktion> findeAlle(Date vonDatum, Date bisDatum, TransaktionsArten art, Kategorie kategorie) {
         return em.createQuery("SELECT t FROM Transaktion t"
-            + " WHERE (t.benutzer = :benutzer)"
-            + " AND (t.art = :art)"
-            + " AND (t.kategorie = :kategorie)"
-            + " AND (t.erstellungsDatum >= :vonDatum) AND (t.erstellungsDatum <= :bisDatum)")
-            .setParameter("benutzer", benutzerBean.gibAktuellenBenutzer())
-            .setParameter("kategorie", kategorie)
-            .setParameter("vonDatum", vonDatum)
-            .setParameter("bisDatum", bisDatum)
-            .setParameter("art", art)
-            .getResultList();
+                + " WHERE (t.benutzer = :benutzer)"
+                + " AND (t.art = :art)"
+                + " AND (t.kategorie = :kategorie)"
+                + " AND (t.erstellungsDatum >= :vonDatum) AND (t.erstellungsDatum <= :bisDatum)")
+                .setParameter("benutzer", benutzerBean.gibAktuellenBenutzer())
+                .setParameter("kategorie", kategorie)
+                .setParameter("vonDatum", vonDatum)
+                .setParameter("bisDatum", bisDatum)
+                .setParameter("art", art)
+                .getResultList();
     }
-    
+
     //Gibt ein StatistikDaten Objekt mit Name der Kategorie und die Summe der Transaktionen zur Kategorie zurück
-    public StatistikDaten getStatistikLastYearPerCategory(TransaktionsArten art){   
+    public StatistikDaten getStatistikLastYearPerCategory(TransaktionsArten art) {
         List<Kategorie> kategorien = kategorieBean.findeAlle(art);
         System.out.println("Anzahl Kategorien " + Integer.toString(kategorien.size()));
-        
+
         StatistikDaten daten = new StatistikDaten();
-        
+
         GregorianCalendar cal = new GregorianCalendar();
         cal.setTime(new Date());
         //heutiger Monat
         int currentMonth = cal.get(Calendar.MONTH);
         //heutiges Jahr
         int currentYear = cal.get(Calendar.YEAR);
-        
+
         System.out.println("Monat " + Integer.toString(currentMonth) + "Jahr " + Integer.toString(currentYear));
-        
-        cal.set(currentYear -1 ,currentMonth +1 , 1);
+
+        cal.set(currentYear - 1, currentMonth + 1, 1);
         Date fromDate = cal.getTime();
-        
+
         System.out.println("fromDate " + fromDate);
-        
-        cal.set(currentYear,currentMonth, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+        cal.set(currentYear, currentMonth, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
         Date toDate = cal.getTime();
-        
+
         System.out.println("toDate " + toDate);
-       
+
         for (Kategorie kategorie : kategorien) {
             System.out.println("Bezeichnung " + kategorie.getBezeichnung());
             daten.setWert(summiereTransaktionen(findeAlle(fromDate, toDate, art, kategorie)), kategorie.getBezeichnung());
         }
-        
+
         return daten;
     }
-    
-    public StatistikDaten getStatistikLastYearPerMonth( TransaktionsArten art, Date date){
+
+    public StatistikDaten getStatistikLastYearPerMonth(TransaktionsArten art, Date date) {
         StatistikDaten sD = new StatistikDaten();
-        
+
         GregorianCalendar cal = new GregorianCalendar();
-        
+
         //Ermitteln des aktuellen Monates und Jahres
         cal.setTime(date);
         int currentMonth = cal.get(Calendar.MONTH);
         int currentYear = cal.get(Calendar.YEAR);
-        
+
         System.out.println("aktueller Monat " + Integer.toString(currentMonth));
         System.out.println("aktuelles Jahr " + Integer.toString(currentYear));
-        
+
         //Es sollen die Werte des letzten Jahres ermittelt werden
         //Setzen des Monates +1 und Jahres -1 als Anfangsdatum
-        int startMonth = currentMonth +1;
-        int startYear = currentYear -1;
-        
-         //Jahreswechsel
-        if (startMonth >= 12){
+        int startMonth = currentMonth + 1;
+        int startYear = currentYear - 1;
+
+        //Jahreswechsel
+        if (startMonth >= 12) {
             startMonth = 0;
-            startYear = startYear +1;
+            startYear = startYear + 1;
         }
-        cal.set(startYear,startMonth , 1);
+        cal.set(startYear, startMonth, 1);
         Date fromDate = cal.getTime();
-        
+
         //Enddatum = heutiges Datum
         Date toDate = date;
-        
+
         System.out.println("von Datum " + fromDate);
         System.out.println("bis Datum " + toDate);
         System.out.println("SQL Abfrage");
-        
+
         //Hier wird eine NativeQuery verwendet, weil SQL Befehle verwendet werden,
         //die so in JPA nicht vorgesehen sind.
-        List<Object[]> results = em.createNativeQuery("select SUM(t.BETRAG) as SUMME, MONTH(t.ERSTELLUNGSDATUM) as MONAT \n" +
-                "from (select * from HAUSHALTSBUCH.TRANSAKTION t  \n" +
-                "where t.ERSTELLUNGSDATUM >= ?1 AND t.ERSTELLUNGSDATUM <= ?2 AND t.BENUTZER_USERNAME = ?3 AND t.ART = ?4) t group by MONTH(t.ERSTELLUNGSDATUM)\n",
+        List<Object[]> results = em.createNativeQuery("select SUM(t.BETRAG) as SUMME, MONTH(t.ERSTELLUNGSDATUM) as MONAT \n"
+                + "from (select * from HAUSHALTSBUCH.TRANSAKTION t  \n"
+                + "where t.ERSTELLUNGSDATUM >= ?1 AND t.ERSTELLUNGSDATUM <= ?2 AND t.BENUTZER_USERNAME = ?3 AND t.ART = ?4) t group by MONTH(t.ERSTELLUNGSDATUM)\n",
                 "MonthTransaktionMapping")
                 .setParameter(1, fromDate)
                 .setParameter(2, toDate)
                 .setParameter(3, benutzerBean.gibAktuellenBenutzer().getUsername())
                 .setParameter(4, art.ordinal())
                 .getResultList();
-        
+
         //Erzeugen eines Double Arrays für die Beträge. Es kann sein, dass in der SQl Abfrage nicht alle Monate repräsentiert werden.
         //Zum Beispiel wenn es aus einem Monat keine Transaktionen gibt.
-        
-        Double [] d = new Double[12];
+        Double[] d = new Double[12];
         System.out.println("Zeilen " + results.size());
         //Die sogenannten Lampda Expressions aus JAVA 8 machen bei der Erzeugung eines Embedded Container
         //Ärger --> IndexOutOfBoundException 
-        
+
         /*
         results.stream().forEach((record) ->{
         System.out.println("Spalten " + record.length);
@@ -184,69 +182,68 @@ public class TransaktionBean extends EntityBean<Transaktion, Long> {
         System.out.println("Betrag " + record[0]);
         System.out.println("Monat " + record[1]);
         });*/
-        
-        for ( Iterator i = results.iterator(); i.hasNext(); ) {
-            Object[] record = (Object [])i.next();
+        for (Iterator i = results.iterator(); i.hasNext();) {
+            Object[] record = (Object[]) i.next();
             System.out.println("Spalten " + record.length);
-        
+
             //Der Wert wird genau auf die passende Position im Array gesetzt. - 0 = Januar
-            d[(Integer)record[1]-1] = (Double)record[0];
-        
+            d[(Integer) record[1] - 1] = (Double) record[0];
+
             System.out.println("Betrag " + record[0]);
             System.out.println("Monat " + record[1]);
         }
-        
+
         //Setzen der Statistik Werte. Es soll nicht fix mit dem Januar begonnen werden, 
         //sondern mit dem aktuellen Monat +1 und aktuelles Jahr -1
         int month = startMonth;
         Double wert;
-        for (int i = 1; i <= 12; i++){
+        for (int i = 1; i <= 12; i++) {
             wert = 0.0;
-            
-            if ( d[month] != null) {
-                wert = d[month]; 
-            } 
-            
+
+            if (d[month] != null) {
+                wert = d[month];
+            }
+
             //Setzen des Statistikwertes
             cal.set(2000, month, 1);
             //Speichern der Daten in ein Statistik Objekt
             sD.setWert(wert, new SimpleDateFormat("MMMM", Locale.GERMAN).format(cal.getTime()));
-            
-            month = month +1;
-            
+
+            month = month + 1;
+
             //Jahreswechsel
-            if (month+1 >= 12){
+            if (month + 1 >= 12) {
                 month = 0;
             }
-        }  
+        }
         return sD;
     }
-   
+
 //<editor-fold defaultstate="collapsed" desc="Alter Code">
     //Gibt ein StatistikDaten Objekt mit Name des Monats und die Summe der Transaktionen zum Monat zurück
-    public StatistikDaten getStatistikLastYearPerMonthOld(TransaktionsArten art){
-        
+    public StatistikDaten getStatistikLastYearPerMonthOld(TransaktionsArten art) {
+
         GregorianCalendar cal = new GregorianCalendar();
         cal.setTime(new Date());
         //heutiger Monat
-        int currentMonth = cal.get(Calendar.MONTH) +1 ;
+        int currentMonth = cal.get(Calendar.MONTH) + 1;
         //heutiges Jahr
         int currentYear = cal.get(Calendar.YEAR);
-        
+
         StatistikDaten daten = new StatistikDaten();
-        
+
         int fromMonth, toMonth;
         Date dateFrom, dateTo;
-        
+
         //Schleife über die Jahre
-        for (int year = currentYear-1; year <= currentYear; year ++){
-            
+        for (int year = currentYear - 1; year <= currentYear; year++) {
+
             //im ersten Jahr: aktueller Monat +1 bis Jahresende
-            fromMonth = currentMonth+1;
+            fromMonth = currentMonth + 1;
             toMonth = 12;
-            
+
             //im zweiten Jahr: Jahresanfang bis aktueller Monat
-            if (currentYear == year){
+            if (currentYear == year) {
                 fromMonth = 1;
                 toMonth = currentMonth;
             }
@@ -254,50 +251,51 @@ public class TransaktionBean extends EntityBean<Transaktion, Long> {
             System.out.print("Year " + Integer.toString(year));
             System.out.print("fromMonth " + Integer.toString(fromMonth));
             System.out.print("toMonth " + Integer.toString(toMonth));
-            
+
             //Schleife über die Monate
-            for (int month = fromMonth; month <= toMonth; month++){
-                
+            for (int month = fromMonth; month <= toMonth; month++) {
+
                 //Setzen des Monates und des Jahres für den ersten des Monates
-                cal.set(year, month-1, 1);
+                cal.set(year, month - 1, 1);
                 dateFrom = cal.getTime();
-                
+
                 //Setzen des Monates und des Jahres für den letzten des Monates
-                cal.set(year, month-1, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+                cal.set(year, month - 1, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
                 dateTo = cal.getTime();
-                
+
                 System.out.print("month " + Integer.toString(month));
                 System.out.print("dateFrom " + dateFrom.toString());
                 System.out.print("dateTo " + dateTo.toString());
-                
+
                 daten.setWert(summiereTransaktionen(findeAlle(dateFrom, dateTo, art)), new SimpleDateFormat("MMMM", Locale.GERMAN).format(cal.getTime()));
             }
         }
-        
+
         return daten;
     }
 //</editor-fold>
 
-    public double summiereTransaktionen(List<Transaktion> transaktionen){
+    public double summiereTransaktionen(List<Transaktion> transaktionen) {
         Double summe = 0.0;
-        for (Transaktion t:  transaktionen){
+        for (Transaktion t : transaktionen) {
             summe = summe + t.getBetrag();
             System.out.print("Summe " + summe);
         }
-        
+
         return summe;
     }
 
     //Methode zum Import von Transaktionen aus einer XML-File
     public void importiereXML(File f, Benutzer aktuellerBenutzer) {
+
         Document doc = null;
 
         try {
             // Das Dokument erstellen
             SAXBuilder builder = new SAXBuilder();
-            
+
             doc = builder.build(f);
-            
+
             XMLOutputter fmt = new XMLOutputter();
 
             // Wurzelelement ausgeben
@@ -326,20 +324,20 @@ public class TransaktionBean extends EntityBean<Transaktion, Long> {
                 Element kategorie = children.get(i).getChild("Kategorie");
 
                 //XML-Werte dem Objekte übergeben
-                if (art.getValue().equals(TransaktionsArten.Ausgabe.getLabel())){
+                if (art.getValue().equals(TransaktionsArten.Ausgabe.getLabel())) {
                     t.setArt(TransaktionsArten.Ausgabe);
                 }
-                if (art.getValue().equals(TransaktionsArten.Einnahme.getLabel())){
+                if (art.getValue().equals(TransaktionsArten.Einnahme.getLabel())) {
                     t.setArt(TransaktionsArten.Einnahme);
                 }
-                
+
+                t.setId(UUID.randomUUID().toString());
                 t.setBenutzer(aktuellerBenutzer);
                 t.setBetrag(Double.valueOf(betrag.getValue()));
                 t.setBeschreibung(beschreibung.getValue());
                 t.setBezeichnung(bezeichnung.getValue());
-               
-    
-                 //Datum anlegen
+
+                //Datum anlegen
                 DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
                 Date startDate = new Date();
                 try {
@@ -358,16 +356,16 @@ public class TransaktionBean extends EntityBean<Transaktion, Long> {
                 } else {
                     //Wenn null -> neue Kategorie anlegen
                     //String bezeichnung, String art, Benutzer benutzer
-                    
+
                     TransaktionsArten tArt = null;
-                    if (art.getValue().equals(TransaktionsArten.Ausgabe.getLabel())){
+                    if (art.getValue().equals(TransaktionsArten.Ausgabe.getLabel())) {
                         tArt = TransaktionsArten.Ausgabe;
                     }
-                    if (art.getValue().equals(TransaktionsArten.Einnahme.getLabel())){
+                    if (art.getValue().equals(TransaktionsArten.Einnahme.getLabel())) {
                         tArt = TransaktionsArten.Einnahme;
                     }
-                
-                    aktuelleKategorie = new Kategorie(kategorie.getValue(),tArt, aktuellerBenutzer);
+
+                    aktuelleKategorie = new Kategorie(kategorie.getValue(), tArt, aktuellerBenutzer);
                     this.kategorieBean.speichernNeu(aktuelleKategorie);
                     t.setKategorie(aktuelleKategorie);
                 }
--- src/main/java/jpa/Transaktion.java
+++ src/main/java/jpa/Transaktion.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.ColumnResult;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.TABLE;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.SqlResultSetMapping;
@@ -13,52 +14,50 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
-
 @Entity
-@SqlResultSetMapping(name = "MonthTransaktionMapping", 
-    columns = {
-        @ColumnResult(name = "SUMME", type = Double.class),
+@SqlResultSetMapping(name = "MonthTransaktionMapping",
+        columns = {
+            @ColumnResult(name = "SUMME", type = Double.class)
+            ,
         @ColumnResult(name = "MONAT", type = Integer.class)
-    }
+        }
 )
 public class Transaktion implements Serializable {
 
     @Id
     //Die Sequenz Tabelle, die für die Transaktion Ids angelegt werden, führt bei
     //der Erzeugung eines Embedded Containers zu Fehler 
-    
-    //@GeneratedValue(generator = "transaktion_ids")
-    //@TableGenerator(name = "transaktion_ids", initialValue = 0, allocationSize = 1)
-    
-    @GeneratedValue
-    private long id;
-   
+//    @GeneratedValue(generator = "transaktion_ids")
+//    @TableGenerator(name = "transaktion_ids", initialValue = 0, allocationSize = 1)
+    //Als Alternative dazu wird UUID verwendet
+    private String id;
+
     //@NotNull(message = "Die Bezeichnung darf nicht leer sein.")
     private String bezeichnung;
-    
+
     private String beschreibung;
-    
-    @NotNull(message= "Bitte geben Sie einen Betrag an.")
+
+    @NotNull(message = "Bitte geben Sie einen Betrag an.")
     private Double betrag = 0.0;
-    
-    @NotNull(message = "Das Datum darf nicht leer sein.") 
+
+    @NotNull(message = "Das Datum darf nicht leer sein.")
     @Temporal(TemporalType.TIMESTAMP)
     private Date erstellungsDatum = new Date();
-    
+
     //@NotNull(message = "Die Art darf nicht leer sein")  
     private TransaktionsArten art;
-   
+
     @ManyToOne
     private Benutzer benutzer = null;
-    
+
     @ManyToOne
     private Kategorie kategorie = null;
-    
+
     //<editor-fold defaultstate="collapsed" desc="Konstruktoren">
-    public Transaktion(){
+    public Transaktion() {
     }
-    
-    public Transaktion(String bezeichnung, String beschreibung, Double betrag, Date erstellungsDatum, TransaktionsArten art, Kategorie kategorie){
+
+    public Transaktion(String bezeichnung, String beschreibung, Double betrag, Date erstellungsDatum, TransaktionsArten art, Kategorie kategorie) {
         this.bezeichnung = bezeichnung;
         this.beschreibung = beschreibung;
         this.betrag = betrag;
@@ -69,69 +68,69 @@ public class Transaktion implements Serializable {
 //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="Getter und Setter">
-    public long getId() {
+    public String getId() {
         return id;
     }
-    
-    public void setId(long id) {
+
+    public void setId(String id) {
         this.id = id;
     }
-    
+
     public String getBezeichnung() {
         return bezeichnung;
     }
-    
+
     public void setBezeichnung(String bezeichnung) {
         this.bezeichnung = bezeichnung;
     }
-    
+
     public String getBeschreibung() {
         return beschreibung;
     }
-    
+
     public void setBeschreibung(String beschreibung) {
         this.beschreibung = beschreibung;
     }
-    
+
     public Double getBetrag() {
         return betrag;
     }
-    
+
     public void setBetrag(Double betrag) {
         this.betrag = betrag;
     }
-    
+
     public Date getErstellungsDatum() {
         return erstellungsDatum;
     }
-    
+
     public void setErstellungsDatum(Date erstellungsDatum) {
         this.erstellungsDatum = erstellungsDatum;
     }
-    
+
     public TransaktionsArten getArt() {
         return art;
     }
-    
+
     public void setArt(TransaktionsArten art) {
         this.art = art;
     }
-    
+
     public Benutzer getBenutzer() {
-    return benutzer;
+        return benutzer;
     }
-    
+
     public void setBenutzer(Benutzer benutzer) {
-    this.benutzer = benutzer;
+        this.benutzer = benutzer;
     }
- 
+
     public Kategorie getKategorie() {
         return kategorie;
     }
-    
+
     public void setKategorie(Kategorie kategorie) {
         this.kategorie = kategorie;
     }
 //</editor-fold>
-    
+
 }
